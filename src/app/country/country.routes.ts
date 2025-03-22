076f94483e2425143218db5ded32f0e4f81fa190
Juan Carlos Avila Meza <avilameza@gmail.com>
2025-03-22 14:01:58 -0500

    Trabajando con los Layouts

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layout/country-layout/country-layout.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 export const countryRoutes: Routes = [
   {
     path:'',
-    component:ByCapitalPageComponent,
+    component:CountryLayoutComponent,
+    children:[
+      {
+        path:'by-country',
+        component:ByCapitalPageComponent
+      },
+      {
+        path:'**',
+        redirectTo:'by-country'
+      }
+    ]
   }
 ];
 
